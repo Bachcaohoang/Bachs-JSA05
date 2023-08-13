b64ba3262a722ef06908b1f718303a2303c9b139
Bachs-JSA05 <bachcaoryan1234@gmail.com>
2023-08-13 12:06:13 +0700

    Buổi 2

---
+++ thu.js
@@ -0,0 +1,31 @@
+//  let x = 0; 
+//  cái phải có
+// // while (x<100) {
+// //     console.log(x += 1);
+// // }
+// kiểm tra điều kiện trước
+// do {
+// console.log(x += 2)
+// }while(x<21);
+// làm trước kiểm tra dk sau
+// for (var i =0; i < 10; i++) {
+//     alert(i)
+// }
+// // nó sẽ ra từ từ cho đến dk
+let mynmber = [0,1,2];
+let letter = [ 'Bách','Hiếu','Tài']
+// console.log(mynmber[1])
+// console.log(letter)
+// for (var i =0; i<=2; i++) {
+//     alert(i +': ' + letter[i])
+// }
+// i++ = i+1
+// let i=0
+// while(i <3)
+// console.log( i+':'+mynmber[i]);
+// i++;
+// let i=0
+// do{
+//     console.log(i+':' + mynmber[1]);
+//     i++;
+// } while(i <3)
