54b77f9d13abf38d838240403d5c953abf626836
Bachs-JSA05 <bachcaoryan1234@gmail.com>
2023-08-20 11:27:16 +0700

    buoi 2

---
+++ thu3.js
@@ -0,0 +1,71 @@
+// let  mynumber = [ 4 ,5 , 7 ,8 , 9, 1]
+// console.log(mynumber)
+//   for( let i = 0; i < mynumber.lenght; i++) {
+//       console.log("gia tri phan tu thu", i, "la:",mynumber[i])
+
+//   }
+// //  mynumber[1] = 6;
+//  console.log(mynumber)
+// thay vi tri cua so
+
+// mynumber.splice(2,1, 7,8);
+// console.log(mynumber)
+
+// console.log(mynumber.length);
+// lấy độ dài cái mảng
+
+// mynumber.push(7)
+// console.log(mynumber)
+// add thêm một số vô mảng tăng độ dài cho mạng và nó thêm vào cuối mảng\
+
+// mynumber.pop()
+// console.log(mynumber)
+// xóa phần tử ở cuối
+
+// mynumber.unshift(9)
+// console.log(mynumber)
+// thêm phần tử cuối lên đầu
+
+// mynumber.shift()
+// console.log(mynumber)
+// xóa phần tử đầu
+
+//  let sports = {
+//      name: 'volleyball,basketball,soccer'
+//      age: '6,60'
+//  }
+//  sports.name = '123'
+//  console.log(sports);
+
+//  sports.action =' walk'
+//  console.log(sports);
+
+//  delete (sports.action)
+//  console.log(sports);
+
+// let cat = new Object({
+//     name: 'tom',
+//     age: 3,
+// enemy:{
+//     name: 'alaska',
+//     age:5
+// }
+// });
+// console.log(cat)
+
+// let dog1 =["alaska",4]
+// let dog2 =["corgi", 5]
+// let dogs=[dog1,dog2]
+// console.log(dogs)
+// console.log( dogs[1][1])
+// // tự nhìn
+
+// let giotraicay =[
+//     ['xoai',2],
+//     ['dua hau',3]
+// ]
+// for( let i = 0; i < giotraicay.lenght; i++) {
+//     console.log("gio trai cay :"+ giotraicay[i][0] + giotraicay[i][1])
+// }
+
+
