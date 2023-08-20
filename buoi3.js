a396d2ce3cd25ad2e6f3a14da13ab59fba875dbd
Bachs-JSA05 <bachcaoryan1234@gmail.com>
2023-08-20 12:02:28 +0700

    buoi 3

---
+++ buoi3.js
@@ -0,0 +1,19 @@
+//  let arr  = [' toan','ngu van','tieng anh']
+// for (let i in arr ){
+//     console.log(arr[i])
+// }
+// for in trong mang
+
+//  let obj = {
+//      'name':"nguyen van dau",
+//      'sex': "khong xac dinh",
+//      'crime':"vi pham ban quyen"
+//  }
+// for (let i in obj)
+// console.log(obj[i])
+// for in obj
+
+//  let arr  = [' toan','ngu van','tieng anh']
+// for (let value of arr)
+// console.log(value)
+// lam dc voi mang ko dc voi obj
